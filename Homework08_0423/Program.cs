3e448fecc416dcb44e5cfb02470fed60e345e044
Artur <Artur@LAPTOP-7VQCC982>
2023-04-08 15:55:41 +0500

    Добавьте файлы проекта.

---
+++ Homework08_0423/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework08_0423
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //3
+            Console.WriteLine("212 F = 100 C");
+            //4
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
